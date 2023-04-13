7b65032b53fdc721ecd8d52b2ae2e78d09218266
ZPhtephen <c.alvaradoruiz02@gmail.com>
2023-04-13 02:48:02 -0600

    carga 3

---
+++ Assets/SceneManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene("Scanner", LoadSceneMode.Single);
+
+        }
+    }
+
+}
